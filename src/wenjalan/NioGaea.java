93b54f77b9f958a8d6d004b1b7e5e66be27a00dc
Alan Wen <36051502+wenjalan@users.noreply.github.com>
2019-05-28 16:11:28 -0700

    Custom Generator Prototype
    Plugin now generates a custom world using only stone.

--- src/wenjalan/NioGaea.java
+++ src/wenjalan/NioGaea.java
@@ -1,6 +1,8 @@
 package wenjalan;
 
+import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.java.JavaPlugin;
+import wenjalan.generators.GaeaGenerator;
 
 public class NioGaea extends JavaPlugin {
 
@@ -12,6 +14,11 @@ public class NioGaea extends JavaPlugin {
     @Override
     public void onDisable() {
         super.onDisable();
+    }
+
+    @Override
+    public ChunkGenerator getDefaultWorldGenerator(String worldName, String id) {
+        return new GaeaGenerator(id);
     }
 
 }
